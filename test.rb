2b7082b69d71cc613d7dc5152abbe4aa1f206c6f
Andrew Santos <asantos@alumni.nd.edu>
2019-07-11 21:59:36 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -3,10 +3,13 @@ require 'yaml'
 def load_library (file)
   emoji = YAML.load_file(file)
   new_hash = {"get_emoticon" => {}, "get_meaning" => {}}
-#  emoji.each {|key, value|
-#    new
-    
-    emoji
+  emoji.each {|key, value|
+    counter=0 
+    while emoji[key][counter] do
+      new_hash["get_meaning"][emoji[key][counter]] = key
+      counter +=1
+    end 
+    new_hash
 end
 
 def get_japanese_emoticon
