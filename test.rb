9e3c78d84a51853ffe53e0c190c257a189d239ba
Andrew Santos <asantos@alumni.nd.edu>
2019-07-11 21:29:05 +0000

    Automatically backed up by Learn

---
+++ test.rb
@@ -0,0 +1,16 @@
+require 'yaml'
+
+def load_library (file)
+  emoji = YAML.load_file(file)
+  return emoji
+end
+
+def get_japanese_emoticon
+  # code goes here
+end
+
+def get_english_meaning
+  # code goes here
+end
+
+puts load_library(lib/translator.rb)
