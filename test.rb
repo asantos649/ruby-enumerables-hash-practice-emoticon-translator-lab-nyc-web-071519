018960a6f2e50b9fa3e920596e37d5e13fe737f8
Andrew Santos <asantos@alumni.nd.edu>
2019-07-11 21:53:48 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -3,7 +3,7 @@ require 'yaml'
 def load_library (file)
   emoji = YAML.load_file(file)
   new_hash = {"get_emoticon" => {}, "get_meaning" => {}}
-#  new_hash["get_meeting"] = emoji
+  new_hash 
     
     
 end
