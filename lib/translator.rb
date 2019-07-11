5e606d694d37abda28ae0f21740181f14c1de12d
Andrew Santos <asantos@alumni.nd.edu>
2019-07-11 22:47:25 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -31,7 +31,7 @@ end
 def get_japanese_emoticon (file, emote)
   new_hash = load_library(file)
   new_hash["get_emoticon"].each {|key, value|
-    return new_hash["get_emoticon"]
+    return key #new_hash["get_emoticon"]
     if new_hash["get_emoticon"][key] == emote
       return "2" #value[emote]
     else
